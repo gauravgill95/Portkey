a262802eac542337ad47dab5606bd2a165106cc0
Gaurav <43656222+gauravgill95@users.noreply.github.com>
2018-11-27 01:55:05 +0530

    Add files via upload

---
+++ js/button.js
@@ -0,0 +1,21 @@
+/* Downloaded from https://www.codeseek.co/ */
+$(function(){
+  $(".increment").click(function(){
+    var count = parseInt($("~ .count", this).text());
+     
+    if($(this).hasClass("up")) {
+      var count = count + 1;
+       
+       $("~ .count", this).text(count);
+    } else {
+      var count = count - 1;
+       $("~ .count", this).text(count);     
+    }
+     
+    $(this).parent().addClass("bump");
+     
+    setTimeout(function(){
+      $(this).parent().removeClass("bump");    
+    }, 400);
+  });
+});
